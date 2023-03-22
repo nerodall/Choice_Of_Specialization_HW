e3b98f1f9bca049749c3dd5b2ea3d1d973ee4c52
Alexey Neroda <neroda.all@mail.ru>
2023-03-23 01:01:22 +0300

    added method PrintArray and LessThanThreeArray and finish the programm

--- Program.cs
+++ Program.cs
@@ -5,13 +5,51 @@ int InputInt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-string [] InpuntArray (string element, int sizeArray)
+string[] InpuntArray(int sizeArray)
 {
-    string [] array = new string [sizeArray];
- for (int i = 0; i < array.Length ; i++)
- {
-    System.Console.WriteLine($"Введите {i+1} элемент массива");
-    array[i]=Console.ReadLine();
- }
- return array;
+    string[] array = new string[sizeArray];
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите {i + 1} элемент массива");
+        array[i] = Console.ReadLine();
+    }
+    return array;
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+
+string[] LessThanThreeArray(string[] array)
+{
+    string[] resultArray = new string[array.Length];
+
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            {
+                resultArray[j] = array[i];
+                j++;
+            }
+        }
+    }
+
+    return resultArray;
+}
+
+
+int sizeArray = InputInt("Введите размер массива");
+string[] array = InpuntArray(sizeArray);
+System.Console.WriteLine("Полученный массив");
+PrintArray(array);
+string[] resultArray = LessThanThreeArray(array);
+System.Console.WriteLine("Новый массив с элементами, длина которых меньше либо равна 3 символа");
+PrintArray(resultArray);
