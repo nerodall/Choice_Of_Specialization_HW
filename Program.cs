54ac75ff96eaeec75c0235025105f29acd0d90bf
Alexey Neroda <neroda.all@mail.ru>
2023-03-23 00:05:44 +0300

    added method input size of array and input array

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+int InputInt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string [] InpuntArray (string element, int sizeArray)
+{
+    string [] array = new string [sizeArray];
+ for (int i = 0; i < array.Length ; i++)
+ {
+    System.Console.WriteLine($"Введите {i+1} элемент массива");
+    array[i]=Console.ReadLine();
+ }
+ return array;
+}
